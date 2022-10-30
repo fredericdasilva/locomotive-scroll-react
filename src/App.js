2f6d48497d7eb8de81700d2e1d084940f0721f9c
fred <fredama03@free.fr>
2022-10-30 23:18:47 +0100

    feat(locomotive): :tada: scroll locomotive example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef(null);
+
+  const options = {
+    smooth: true,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <LocomotiveScrollProvider options={options} containerRef={ref}>
+      <main data-scroll-container ref={ref}>
+        <section
+          className="intro"
+          data-scroll //This attribute makes this section an independent scrollable container
+          data-scroll-speed="4"
+          data-scroll-section
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h1>This is the Introduction section</h1>
+        </section>
+
+        <section className="contents" data-scroll-section>
+          <h1
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="9"
+          >
+            I Love React
+          </h1>
+          <h1
+            data-scroll
+            data-scroll-direction="vertical"
+            data-scroll-speed="9" // Values provided here affect the animations
+          >
+            That's why I code every day and this great
+          </h1>
+        </section>
+
+        <section className="footer" data-scroll-section>
+          <h1>Let's end the application with this Footer</h1>
+        </section>
+      </main>
+    </LocomotiveScrollProvider>
   );
 }
-
 export default App;
