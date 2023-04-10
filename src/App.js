c0d62b8aa6ca1ed92b8f198d1b6eb2182f69753e
fredericdasilva <52976188+fredericdasilva@users.noreply.github.com>
2023-04-10 19:41:21 +0200

    feat3 + update text

--- src/App.js
+++ src/App.js
@@ -18,12 +18,10 @@ function App() {
           data-scroll-speed="4"
           data-scroll-section
         >
-
           <h1>
-            This is the Introduction section from from hotfix2 + new version + feat2 + another update from
-            feat2
+            This is the Introduction section from from hotfix2 + new version +
+            feat2 + another update from feat2 + feat 3
           </h1>
-
         </section>
 
         <section className="contents" data-scroll-section>
